f9652b47f69cdb3795e39c2e051880b280a0a63c
Brijesh Kumar Singh <Brijesh Kumar Singh@DESKTOP-FGH3QIG.(none)>
2021-02-08 23:50:35 +0530

    Some Changes to scrape all the pages

--- newspider/spiders/SpiderQuotes.py
+++ newspider/spiders/SpiderQuotes.py
@@ -3,12 +3,20 @@ class QuotesSpider(scrapy.Spider):
     name="quotes_spider"
     def start_requests(self):
         urls=['http://quotes.toscrape.com/page/1/',
-               'http://quotes.toscrape.com/page/2/',]
+             ]
         for url in urls:
             yield scrapy.Request(url=url,callback=self.parse)
     def parse(self,response):
-        page=response.url.split("/")[-2]
-        filename='quotes-%s' % page
-        with open(filename,'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for q in response.css("div.quote"):
+            text=q.css('span.text::text').get()
+            author=q.css('small.author::text').get()
+            tags=q.css('a.tag::text').getall()
+            yield{
+                'text':text,
+                'author':author,
+                'tags':tags
+            }
+        next_page=response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page=response.urljoin(next_page)
+            yield scrapy.Request(next_page,callback=self.parse)
